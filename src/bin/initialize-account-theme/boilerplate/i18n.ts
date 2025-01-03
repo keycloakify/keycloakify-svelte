7aa088af255b44be3b91d459ac31d8566d77c8f4
Joseph Garrone <joseph.garrone.gj@gmail.com>
2025-01-03 22:59:28 +0100

    Update account boilerplate

--- src/bin/initialize-account-theme/boilerplate/i18n.ts
+++ src/bin/initialize-account-theme/boilerplate/i18n.ts
@@ -1,11 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { i18nBuilder } from '@keycloakify/svelte/account';
 import type { ThemeName } from '../kc.gen';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { useI18n, ofTypeI18n } = i18nBuilder
-  .withThemeName<ThemeName>()
-  .withExtraLanguages({}) // See: https://docs.keycloakify.dev/i18n/adding-support-for-extra-languages
-  .withCustomTranslations({}) // See: https://docs.keycloakify.dev/i18n/adding-new-translation-messages-or-changing-the-default-ones
-  .build();
+/** @see: https://docs.keycloakify.dev/features/i18n */
+const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+
 type I18n = typeof ofTypeI18n;
 export { useI18n, type I18n };
