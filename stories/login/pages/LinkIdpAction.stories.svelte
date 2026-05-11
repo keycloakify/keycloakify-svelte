ecb485aa003e9b4d8c36941b02c81f559fafaa35
Luca Peruzzo <peruzzoluca98@gmail.com>
2026-05-11 08:42:31 +0200

    fix: missing stories
    Ref: https://github.com/keycloakify/keycloakify-starter-svelte/issues/6

---
+++ stories/login/pages/LinkIdpAction.stories.svelte
@@ -0,0 +1,37 @@
+<script
+  context="module"
+  lang="ts"
+>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import type { KcPageStoryProps } from '../KcPageStory';
+  import KcPageStory from '../KcPageStory.svelte';
+
+  const args: KcPageStoryProps = { pageId: 'link-idp-action.ftl' };
+  const { Story } = defineMeta({
+    title: 'login/link-idp-action.ftl',
+    component: KcPageStory,
+    args: args,
+  });
+</script>
+
+<Story
+  name="Default"
+  args={{
+    ...args,
+    kcContext: {
+      idpDisplayName: 'GitHub',
+      url: { loginAction: '/mock-login-action' },
+    },
+  }}
+/>
+
+<Story
+  name="DifferentProvider"
+  args={{
+    ...args,
+    kcContext: {
+      idpDisplayName: 'Google',
+      url: { loginAction: '/custom-login-action' },
+    },
+  }}
+/>
---
+++ stories/login/pages/SelectOrganization.stories.svelte
@@ -0,0 +1,17 @@
+<script
+  context="module"
+  lang="ts"
+>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import type { KcPageStoryProps } from '../KcPageStory';
+  import KcPageStory from '../KcPageStory.svelte';
+
+  const args: KcPageStoryProps = { pageId: 'select-organization.ftl' };
+  const { Story } = defineMeta({
+    title: 'login/select-organization.ftl',
+    component: KcPageStory,
+    args: args,
+  });
+</script>
+
+<Story name="Default" />
